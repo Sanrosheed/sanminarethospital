ab76a4812669d17edca5ec03aae8cb97c897fec7
sanrosheed <sanrosheed01@gmail.com>
2026-03-01 14:19:11 +0100

    Address and update

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import medicallogo from "../../public/blackdocs.png";
+import medicallogo from "../../public/MinHero1.jpg";
 import Link from "next/link";
 import logo2 from "../../public/Sanminaret_logo4.png";
 
@@ -9,14 +9,21 @@ const Hero = () => {
     <section className="p-10 py-20 md:py-24 text-center">
       <div className="container mx-auto flex flex-col lg:flex-row items-center">
         <div className="lg:w-1/2">
-          <Image src={logo2} className="w-52 h-32 md:w-80 md:h-52 ml-10 md:ml-28" alt="Logo" />
+          <Image
+            src={logo2}
+            className="w-52 h-32 md:w-80 md:h-52 ml-10 md:ml-28"
+            alt="Logo"
+          />
           <p className="mt-4 md:mt-2 text-lg">
-          We provide the best & affordable health care. Our goal is to deliver the highest quality healthcare services. We believe that everyone deserves access to excellent medical care without compromising on quality.
+            We provide the best & affordable health care. Our goal is to deliver
+            the highest quality healthcare services. We believe that everyone
+            deserves access to excellent medical care without compromising on
+            quality.
           </p>
           <Link href="#appointment">
-          <button className="mt-6 bg-blue-600 text-white px-6 py-3 rounded-md hover:bg-blue-700">
-            Book Appointment
-          </button>
+            <button className="mt-6 bg-blue-600 text-white px-6 py-3 rounded-md hover:bg-blue-700">
+              Book Appointment
+            </button>
           </Link>
         </div>
         <div className="lg:w-1/2 mt-8 lg:mt-0">
